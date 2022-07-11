fbb703899e12f28ea7a268bd2723094396e10c3c
Marcos Lanzoni <marcoslanzoni@gmail.com>
2022-07-10 21:01:07 -0300

    erro no sequelize ver git add .!

--- db/conection.js
+++ db/conection.js
@@ -1,6 +1,6 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./db/app.db",
-});
-module.exports = sequelize;
+// const Sequelize = require("sequelize");
+// // const sequelize = new Sequelize({
+// //   dialect: "sqlite",
+// //   storage: "./db/app.db",
+// // });
+// module.exports = sequelize;
